8ebb86032e0466bd0ddc0ecf59daef3219904681
Luiz Eduardo da Silva <dasilvaeduardo.ti@gmail.com>
2022-11-09 21:49:08 -0300

    Implementando um exemplo de middlewares

--- 04_Express/index.js
+++ 04_Express/index.js
@@ -4,6 +4,20 @@ const app       = express();
 const porta     = 3000; //Porta para acesso ao servidor
 const basePath  = path.join(__dirname, 'templates');
 
+const checarAutenticacao = function (req, res, next){
+    req.authStatus = true;
+
+    if (req.authStatus){
+        console.log("Usuário logado!");
+        next();
+    }else{
+        console.log("Usuário não está logado! Favor fazer login!");
+        next();
+    }
+}
+
+app.use(checarAutenticacao);
+
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${basePath}/index.html`);
 });
