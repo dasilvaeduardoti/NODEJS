84f9e9afd70f3ff520ec5e0aa76bc0e2cb905e4d
Luiz Eduardo da Silva <dasilvaeduardo.ti@gmail.com>
2022-11-30 21:54:49 -0300

    Lista tarefas

--- 08_MVC/controllers/TarefaController.js
+++ 08_MVC/controllers/TarefaController.js
@@ -4,4 +4,21 @@ module.exports = class TarefaController {
     static criarTarefa(req, res) {
         res.render('tarefas/criar-tarefas');
     }
+
+    static async salvarTarefa(req, res) {
+        const tarefaNova = {
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            status: false
+        };
+        await Tarefa.create(tarefaNova);
+        res.redirect('/tarefas');
+    }
+
+    static async listarTarefas(req, res){
+
+        const tarefas = await Tarefa.findAll({raw: true});
+
+        res.render('tarefas/listar-tarefas', {tarefas});
+    }
 }
--- 08_MVC/routes/tarefasRoutes.js
+++ 08_MVC/routes/tarefasRoutes.js
@@ -2,7 +2,11 @@ const express           = require('express');
 const router            = express.Router();
 const TarefaController  = require('../controllers/TarefaController');
 
+//Rota responsável por exibir todas as tarefas
+router.get('/', TarefaController.listarTarefas);
 //Rota responsável por exibir o formulário de cadastro de tarefas
 router.get('/criar', TarefaController.criarTarefa);
+//Rota responsável por salvar os dados da tarefa no banco de dados
+router.post('/save', TarefaController.salvarTarefa);
 
 module.exports = router;
